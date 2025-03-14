c9bb6a690639290f914d438c5c82cbcc7e64ce3d
Angelo  Britto <abrittojose@gmail.com>
2025-03-14 11:26:29 -0500

    Improve menu main.py

--- src/main.py
+++ src/main.py
@@ -8,24 +8,46 @@ from modules.get_scopus_references import get_scopus_references
 def preprocesing_df(path_wos=None,path_scopus=None):
     
     if path_wos:
+        print("""
+              ============================
+                Leyendo archivo de WoS...
+              ============================
+              """)
         # Dataframe	
         wos_df = wos_txt_to_df(path_wos)
+        print("1. Dataframe de WoS hecho")
         # Remove duplicates
         wos_df = remove_duplicates_df(wos_df)
+        print("2. Duplicados removidos")
         # Get references
+        print("3. Referencias de WoS hecha")
         wos_references = get_wos_references(wos_df)
+    else:
+        print("""
+              No se ha ingresado un archivo de WoS
+              """)
     
     
     if path_scopus:
+        print("""
+              ===============================
+                Leyendo archivo de Scopus...
+              ===============================
+              """)
         # Dataframe
+        print("1. Dataframe de Scopus hecho")
         scopus_df = scopus_csv_to_df(path_scopus)
         # Remove duplicates
-        print(f"Estas son las columnas al princicio {scopus_df.columns}\n\n")
+        print("2. Duplicados removidos")
         scopus_df = remove_duplicates_df(scopus_df)
         # Get references
+        print("3. Referencias de Scopus hecha")
         scopus_references = get_scopus_references(scopus_df)
-        print(scopus_references)
-    
+    else:
+        print("""
+              No se ha ingresado un archivo de Scopus
+              """)
+        
  
     
     
