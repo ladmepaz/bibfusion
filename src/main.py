0c96a2d1bdcea18799419ac884e10c6201fe1a8a
Angelo  Britto <abrittojose@gmail.com>
2025-03-23 19:01:14 -0500

    Add merge_wos function

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from modules.duplicates import remove_duplicates_df
 from modules.get_wos_references import get_wos_references
 from modules.get_scopus_references import get_scopus_references
 from modules.enrich_wos_ref import update_wos_ref_with_crossref
-
+#from modules.enrich_scopus_ref import enrich_scopus_ref
+from modules.merge_wos_ref import merge_wos_ref
 
 def preprocesing_df(path_wos=None,path_scopus=None):
     
@@ -26,7 +27,7 @@ def preprocesing_df(path_wos=None,path_scopus=None):
         # Enrich references with Crossref
         print("4. Referencias de WoS enriquecidas con Crossref")
         enrich_wos_ref = update_wos_ref_with_crossref(wos_references,'doi')
-        
+        merge_wos = merge_wos_ref(wos_df,enrich_wos_ref) 
         enrich_wos_ref.to_csv('tests/files/enrich_wos_ref.csv', index=False)
         
         
@@ -52,8 +53,8 @@ def preprocesing_df(path_wos=None,path_scopus=None):
         # Get references
         print("3. Referencias de Scopus hecha")
         scopus_references, scopus_df = get_scopus_references(scopus_df)
-        # print(scopus_references.columns)
-        # print(scopus_df.columns)
+        
+        print("4. Referencias de Scopus Enriquecidas con Crossref")
     else:
         print("""
               No se ha ingresado un archivo de Scopus
