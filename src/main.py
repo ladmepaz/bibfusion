3f1410d4b320cc3537f7d3cd9f3a22477217585b
Angelo  Britto <abrittojose@gmail.com>
2025-06-05 15:45:54 -0500

    Create output directory for WoS results and update file paths for processed data

--- src/main.py
+++ src/main.py
@@ -41,8 +41,14 @@ from modules.unify_author_fullname_and_orcid import unify_author_fullname_and_or
 
 import pandas as pd
 
+import os
+
 def preprocesing_df(path_wos=None,path_scopus=None):
-    
+    PATH = "WoS_results"
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
+
     if path_wos:
         print("""
               ============================
@@ -52,29 +58,32 @@ def preprocesing_df(path_wos=None,path_scopus=None):
         
         # Dataframe	
         wos_df = wos_txt_to_df(path_wos)
-        wos_df.to_csv('tests/files/WoS/1_temp_wos_df.csv', index=False)
+        # wos_df.to_csv('tests/files/WoS/1_temp_wos_df.csv', index=False)
         print("1. Dataframe de WoS hecho")
         
         # Remove duplicates
         wos_df = remove_duplicates_df(wos_df)
-        wos_df.to_csv('tests/files/WoS/2_temp_wos_df_removeDuplicates.csv', index=False)
+        # wos_df.to_csv('tests/files/WoS/2_temp_wos_df_removeDuplicates.csv', index=False)
         print("2. Duplicados removidos")
 
         # Get references
         wos_references, wos_citation = get_wos_references(wos_df)
-        wos_references.to_csv('tests/files/WoS/3_temp_wos_references.csv', index=False)
-        wos_df.to_csv('tests/files/WoS/3_temp_wos_df.csv', index=False)
+        wos_citation.to_csv('tests/files/WoS_results/Citation.csv', index=False)
+        # wos_references.to_csv('tests/files/WoS/3_temp_wos_references.csv', index=False)
+
         print("3. Referencias de WoS hecha")
         
         # Enrich references with Openalex
-        enrich_wos_ref = pd.read_csv('tests/files/WoS/enrich_wos_ref.csv')
+        wos_ref_enriched = enrich_references_with_openalex(wos_references)
+        wos_ref_enriched.to_csv('tests/files/WoS_results/enrich_wos_ref.csv')
+        # enrich_wos_ref = pd.read_csv('tests/files/WoS/enrich_wos_ref.csv')
         print("4. Referencias de WoS enriquecidas con OpenAlex")
       
 
         # Merge WoS and references
-        wos_df_3 = merge_wos_ref(wos_df, enrich_wos_ref)
+        wos_df_3 = merge_wos_ref(wos_df, wos_ref_enriched)
         print("5. Dataframe de WoS y referencias unidos")
-        wos_df_3.to_csv('tests/files/WoS/5_temp_wos_df_merged.csv', index=False)
+        # wos_df_3.to_csv('tests/files/WoS/5_temp_wos_df_merged.csv', index=False)
         
         ##############################################
         #           Scimago Dataframe
@@ -84,24 +93,25 @@ def preprocesing_df(path_wos=None,path_scopus=None):
 
         wos_df_4 = standarize_journal_data(wos_df_3)
         print("5.1. Estandarización de datos de revistas WoS")
-        wos_df_4.to_csv('tests/files/WoS/5_temp_wos_df_standarized.csv', index=False)
+        # wos_df_4.to_csv('tests/files/WoS/5_temp_wos_df_standarized.csv', index=False)
 
 
         # fill missing issn and eissn with Scimago
         wos_df_5 = fill_missing_issn_eissn_with_scimago(wos_df_4, scimago)
         print("6. Rellenado de ISSN/EISSN faltantes con Scimago")
-        wos_df_5.to_csv('tests/files/WoS/6_temp_wos_df_fixmissingjournalreferences.csv', index=False)
+        # wos_df_5.to_csv('tests/files/WoS/6_temp_wos_df_fixmissingjournalreferences.csv', index=False)
 
         wos_df_6 = aggregate_sr_and_attach_scimago_ids(wos_df_5, scimago)
         print("6.1. Agregado SR y adjuntado IDs de Scimago")
-        wos_df_6.to_csv('tests/files/WoS/6_temp_wos_df_aggregated.csv', index=False)
+        # wos_df_6.to_csv('tests/files/WoS/6_temp_wos_df_aggregated.csv', index=False)
 
         journal, scimago_raw = resolve_duplicate_sourceids(wos_df_6)
         print("6.2. Resuelto IDs de fuente duplicados en WoS")
-        journal.to_csv('tests/files/WoS/6_temp_wos_journal.csv', index=False)
-        scimago_raw.to_csv('tests/files/WoS/6_temp_scimago_raw.csv', index=False)
+        journal.to_csv('tests/files/WoS_results/Journal.csv', index=False)
+        # scimago_raw.to_csv('tests/files/WoS/6_temp_scimago_raw.csv', index=False)
         
         scimagodb = add_year_and_scimago_info(scimago_raw, wos_df_3, scimago)
+        scimagodb.to_csv('tests/files/WoS_results/scimagodb.csv', index=False)
 
 
 
@@ -114,30 +124,30 @@ def preprocesing_df(path_wos=None,path_scopus=None):
         # Get author data
         wos_author_raw = get_wos_author_data(wos_df_3)
         print("8. Generado 'wos_author_raw'")
-        wos_author_raw.to_csv('tests/files/WoS/8_temp_wos_author_raw.csv', index=False)
+        # wos_author_raw.to_csv('tests/files/WoS/8_temp_wos_author_raw.csv', index=False)
         
         # Enrich author data
         wos_author_enriched = enrich_wos_author_data(wos_author_raw)
         print("9. Enriched 'wos_author_raw'")
-        wos_author_enriched.to_csv('tests/files/WoS/9_temp_wos_author_enriched.csv', index=False)
+        # wos_author_enriched.to_csv('tests/files/WoS/9_temp_wos_author_enriched.csv', index=False)
         
         # Merge WoS and author data
         wos_author, articleauthor_wos, wos_author_affiliation_no_country = unify_author_fullname_and_orcid(wos_author_enriched)
         print("10. Generado wos_author, articleauthor_wos y wos_author_affiliation")
-        wos_author.to_csv('tests/files/WoS/10_temp_wos_author.csv', index=False)
-        articleauthor_wos.to_csv('tests/files/WoS/10_temp_articleauthor_wos.csv', index=False)
-        wos_author_affiliation_no_country.to_csv('tests/files/WoS/10_temp_wos_author_affiliation.csv', index=False)
+        wos_author.to_csv('tests/files/WoS_results/Author.csv', index=False)
+        articleauthor_wos.to_csv('tests/files/WoS_results/Articleauthor_wos.csv', index=False)
+        # wos_author_affiliation_no_country.to_csv('tests/files/WoS/10_temp_wos_author_affiliation.csv', index=False)
 
         # Get country affiliation
         country_codes_file = r"tests\files\country.csv"
         affiliation_0 = fill_missing_affiliations(wos_author_affiliation_no_country)
         affiliation = extract_countries(affiliation_0, country_codes_file)
         print("11. Paises de afiliación extraídos")
-        affiliation.to_csv('tests/files/WoS/11_temp_wos_country.csv', index=False)
+        affiliation.to_csv('tests/files/WoS_results/Affiliation.csv', index=False)
         
         article = get_article_entity(wos_df_3)
         print("12. Entidad de artículo obtenida")
-        article.to_csv('tests/files/WoS/12_temp_wos_article.csv', index=False)
+        article.to_csv('tests/files/WoS_results/Article.csv', index=False)
 
      
     else:
