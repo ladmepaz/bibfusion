bf82a7d45e6747f1245fee32b8379b088e627e36
zhezter <zhezter.dev@gmail.com>
2026-03-23 15:48:13 -0500

    fix unintended change in __main__.py

--- src/bibfusion/__main__.py
+++ src/bibfusion/__main__.py
@@ -3,10 +3,10 @@ from .pipeline import preprocessing_df
 
 def main():
     preprocessing_df(
-        path_wos=r"data/WoS_303.txt",
-        path_scopus=r"data/Scopus_339.csv",
-        path_scimago=r"data/scimago.csv",
-        path_country=r"data/country.csv",
+        path_wos=r"PATH/TO/YOUR/FILE/wos_data.txt",
+        path_scopus=r"PATH/TO/YOUR/FILE/scopus_data.csv",
+        path_scimago=r"data\scimago.csv",
+        path_country=r"data\country.csv",
         API_KEY_OPENALEX="YOUR_OPENALEX_API_KEY",
     )
 
